2424e041455aca82b581097b868181e6b8e007c3
shubhamkumarpandey <shubhamkumarpande@gmail.com>
2018-11-23 15:24:17 +0530

    First java program

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * First Java program, which says hello.
+ */
+public class Hello {   
+// Save as "Hello.java"
+   public static void main(String[] args) { 
+ // Program entry point
+      System.out.println("Hello, world!");   
+// Print text message
+   }
+}
